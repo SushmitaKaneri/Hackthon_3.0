18750f07cdea693f560e2863cd7d814665ec1b67
Sushmita <>
2019-11-11 00:33:35 +0530

    Password

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87e2e1ec95406a869f86d617080386e0.js"
+  "/precache-manifest.3a9f2c46a75beacde32cf01baf72bf82.js"
 );
 
 workbox.clientsClaim();
